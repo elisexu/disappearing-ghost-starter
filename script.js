36e2e4e8a82891bff859eb40857bce1feb6ea1ed
Glitch (elisexu-disappearing-ghost-starter) <none>
2020-12-17 00:13:44 +0000

    📹⛺️ Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+$(".hide-button").click(function() {
+  $("img").hide();
+})
 
+$(".show-button").click(function() {
+  $("img").show();
+})
+
+$(".update-img-button").click(function() {
+  $("img").attr("src", "https://cdn.glitch.com/db6e707c-f072-4f0d-a9bc-d8a0553fcd3d%2Fcuteghosts.jpg?v=1604513586026")
+});
+
+$(".message-button").click(function() {
+  let input = $("input").val();
+  $(".message").append("<p>"+ input + "<p>");
+});
+
+$(".name-button").click(function() {
+  let name = $("input").val();
+  $("h1").text("My name is " + name);
+});
